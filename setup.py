66b00397d0267922e8689ef6d561c670345ebe0a
Craig Weber <crgwbr@gmail.com>
2024-05-22 15:27:58 -0400

    Get tests running in Gitlab CI

--- setup.py
+++ setup.py
@@ -3,7 +3,15 @@
 from setuptools import setup
 
 setup(
-    setup_requires=['pbr'],
-    install_requires=['requests>=1.1'],
+    setup_requires=["pbr"],
+    install_requires=[
+        # Requests >=2.32.2 blocked by:
+        # https://github.com/msabramo/requests-unixsocket/pull/72
+        "requests>=1.1,<2.32.2",
+        # urllib3 >= 2 is currently blocked by this issue:
+        # - https://github.com/msabramo/requests-unixsocket/issues/70
+        # - https://github.com/msabramo/requests-unixsocket/pull/69
+        "urllib3<2",
+    ],
     pbr=True,
 )
